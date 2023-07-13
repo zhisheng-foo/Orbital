d5dd1767977e707832880e729be0f01595463ec5
soulblast <dave9938@gmail.com>
2023-07-14 07:36:53 +0800

    Animation and Audio Complete
    Subtitles (Bottom text) has to be updated

---
+++ Assets/Victory/Script/BGMctrl.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMctrl : MonoBehaviour
+{
+    public AudioClip BGM;
+    public AudioClip spider;
+
+    private AudioSource audiosource;
+
+    private bool isPaused = false;
+    private float pauseTime = 11.3f;
+    private float currentTime = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+        StartCoroutine(PlayAudioClips());
+    }
+
+    IEnumerator PlayAudioClips()
+    {
+        audiosource.clip = spider;
+        audiosource.Play();
+
+        yield return new WaitForSeconds(spider.length);
+
+        audiosource.clip = BGM;
+        audiosource.Play();
+
+        while (currentTime < pauseTime)
+        {
+            if (!isPaused)
+                currentTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        audiosource.Pause();
+
+        // Resume playing after the pause time
+        while (audiosource.time < BGM.length)
+        {
+            if (!isPaused)
+                audiosource.UnPause();
+            else
+                audiosource.Pause();
+
+            yield return null;
+        }
+    }
+
+    // Method to pause/resume the audio
+    public void PauseResumeAudio()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+            audiosource.Pause();
+        else
+            audiosource.UnPause();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
